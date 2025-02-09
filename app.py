6198e47ac37ddca3e246d1b27bc9039e8dfe88ff
Sarah <sarah.i.rubenstein@gmail.com>
2025-02-09 14:08:00 -0600

    json instead of HTML and error handling

--- app.py
+++ app.py
@@ -1,24 +1,33 @@
-from flask import Flask
-from flask import request
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    data = {"message": "specify a route like /api/[information]"}
+    return jsonify(data), 200
 
 # returns a list of all building names
 @app.route('/api/building-names', methods = ['GET'])
 def getBuildingNames():
-    return "<p>should return building names</p>"
+    data = {"placeholder_message": "should return building names"}
+    return jsonify(data), 200
 
 # returns details for a specific building
 @app.route('/api/building/<id>', methods = ['GET'])
 def getBuildingDetails(id):
-    return f'<p>Building ID: {id} </p>' 
+    data = {"placeholder_message": f'Building ID: {id}'}
+    return jsonify(data), 200
 
 # returns university information
 @app.route('/api/university', methods = ['GET'])
 def getUniversityInfo():
-    return "<p>Should return university information</p>"
+    data = {"placeholder_message": "Should return university information"}
+    return jsonify(data), 200
+
+# error handling
+@app.errorhandler(404)
+def page_not_found(error):
+    data = {"message": "page not found"}
+    return jsonify(data), 404
 
